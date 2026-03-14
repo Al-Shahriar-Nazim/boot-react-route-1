55b88553f92d9d4061cb0fc8c600b527d649803a
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-03-14 23:54:24 +0600

    added useNaviation hook implement

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import React from "react";
+import { Outlet, useNavigation } from "react-router";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const navigation = useNavigation();
+  const isNavigation = Boolean(navigation.location)
+
+  return (
+    <div>
+      <Header></Header>
+      <div>
+        {
+            isNavigation && <span>Loading.....</span>
+        }
+        <Outlet></Outlet>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
